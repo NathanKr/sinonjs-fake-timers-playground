5732a1c45e5b7ca074496aeda199ac3ea3da725e
Nathan Krasney <natankrasney@gmail.com>
2023-06-27 07:08:17 +0300

    add return type

--- src/time-utils.ts
+++ src/time-utils.ts
@@ -1,11 +1,11 @@
-export function pauseMs(sleepMs: number) {
+export function pauseMs(sleepMs: number): Promise<unknown> {
   return new Promise((resolve) => {
     setTimeout(resolve, sleepMs);
   });
 }
 
-export function isItDecember() {
+export function isItDecember() : boolean{
   const currentDate = new Date();
   const monthZeroBased = currentDate.getMonth();
-  return (monthZeroBased == 11)
+  return monthZeroBased == 11;
 }
